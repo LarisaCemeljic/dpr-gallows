0f34b01b4ff861914775c6d0a84c8e41a46cb40f
Goran Petković <goran.petkovic@vsite.hr>
2017-01-06 12:01:09 +0100

    Changing few features

--- Hangman/admin.py
+++ Hangman/admin.py
@@ -3,4 +3,4 @@ from django.contrib import admin
 from .models import Word, Highscore
 
 admin.site.register(Word)
-#admin.site.register(Highscore)
+admin.site.register(Highscore)
--- Hangman/models.py
+++ Hangman/models.py
@@ -1,9 +1,21 @@
 from django.db import models
 
+
 class Highscore(models.Model):
-		 player_name = models.CharField(max_length=200)
-		 highscore_date = models.DateTimeField('Highscore dete');
-		 score = models.IntegerField(default=0)
+    verbose_name = "Highscore"
+    verbose_name_plural = "Highscores"
+    player_name = models.CharField(max_length=200)
+    highscore_date = models.DateTimeField('Highscore dete')
+    score = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.player_name
+
 
 class Word(models.Model):
-		 word = models.CharField(max_length=200)
+    verbose_name = "Word"
+    verbose_name_plural = "Words"
+    word = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.word
